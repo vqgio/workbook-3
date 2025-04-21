592cf0290dd1795e3dcb1ef81834e736f21f402f
Gio <giovannizavala82@gmail.com>
2025-04-21 15:40:12 -0500

    finished quotes, working on try catch

---
+++ FamousQuotes/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] famousQuotes = {
+        "timbob",
+        "jimmybob",
+        "johnny",
+        "carl",
+        "melch",
+        "alex",
+        "carl",
+        "gio",
+        "cesar",
+        "leo"
+        };
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Pick a number between 1-10 for the quote:");
+        int userChoice = scanner.nextInt();
+        System.out.println("selected quote: " + famousQuotes[userChoice - 1]);
+//        catch (IllegalArgumentException e) {
+//            System.out.println(e.getMessage());
+//        catch (Exception e) {
+//                System.out.println("ERROR");
+//                Scanner.nextLine();
+//            }
+        }
+    }
+}
